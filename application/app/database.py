12fb43b93fd958d5d4a052bae8e7f1b20ac3dc28
NoPasaRun <bogdanbelenesku@gmail.com>
2023-01-09 20:05:34 +0300

    modified connection to data base

--- application/app/database.py
+++ application/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-engine = create_engine('postgres+psycopg2://bogdan:YxrEvXddCIfsqgF7DNnFbnq8I498nRlh@dpg-ceu4cv5a4990mi8opjl0-a.frankfurt-postgres.render.com/poker_db')
+engine = create_engine('postgresql+psycopg2://bogdan:YxrEvXddCIfsqgF7DNnFbnq8I498nRlh@dpg-ceu4cv5a4990mi8opjl0-a.frankfurt-postgres.render.com/poker_db')
 Session = sessionmaker(bind=engine)
 db_session = Session()
 Base = declarative_base(bind=engine)
